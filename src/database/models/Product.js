156f69c14ae6976fd3b7b848072d9917d6efa2f9
Jona <retratos.jonatan@gmail.com>
2021-10-01 20:11:33 -0300

    Models

---
+++ src/database/models/Product.js
@@ -0,0 +1,48 @@
+module.exports = function(sequelize, dataTypes){
+    let alias = "Product";
+    let cols = {
+        id: {
+            type: dataTypes.INTEGER(11).UNSIGNED,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        name: {
+            type: dataTypes.STRING(45),
+            allowNull: false
+        },
+        price:{
+            type: dataTypes.INTEGER(11),
+            allowNull: false
+        },
+        discount: {
+            type: dataTypes.INTEGER(11),
+        },
+        description: {
+            type: dataTypes.STRING(800),
+        },
+        subcategoryId: {
+            type: dataTypes.INTEGER(11),
+            allowNull: false
+        }
+    }
+    let config = {
+        tableName: "products", //No hace falta
+        timestamps: true
+    }
+
+    const Product = sequelize.define(alias, cols, config)
+
+    Product.associate = models => {
+        Product.belongsTo(models.Subcategory, {
+            as: "subcategory",
+            foreignKey: "subcategoryId"
+        })
+        Product.hasMany(models.ProductImage, {
+            as: "images",
+            foreignKey: "productId"
+        })
+    }
+
+    return Product
+}
